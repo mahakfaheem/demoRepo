d0d3ba88a3cc9b2132bc027a21fc9af7958f57f7
Ahana99Nair <ahana.nair22@st.niituniversity.in>
2024-01-13 19:30:23 +0530

    adding the sub and div

--- python/calc.py
+++ python/calc.py
@@ -5,8 +5,19 @@ def sum(a,b):
 def mul(a,b):
     return(a*b)
 
+
+def sub(a, b):
+    return(a-b)
+
+def div(a, b):
+    return(a/b)
+
+
+
 a=10
 b=11
 
 s = sum(a,b)
 m = mul(a,b)
+su=sub(a,b)
+d=div(a,b)
